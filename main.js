4477324cb159d7142e16d0a4b5a2235c93b5c57d
Nisheed Jagadish <nisjagad@publicisgroupe.net>
2018-05-25 16:28:56 +0530

    Added form and error handling

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const url = require('url');
-var SerialPort = require('serialport');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const url = require("url");
+var SerialPort = require("serialport");
 const Readline = SerialPort.parsers.Readline;
-var port = new SerialPort('/dev/cu.SLAB_USBtoUART', {
-  baudRate: 9600
-}); 
-
-const parser = new Readline(); 
-port.pipe(parser); 
-parser.on('data', console.log);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -17,84 +10,110 @@ let win, winSize;
 const step = 40;
 
 function createWindow() {
-    // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 });
+  // Create the browser window.
+  win = new BrowserWindow({ width: 800, height: 600 });
+  winSize = win.getSize();
+
+  // and load the index.html of the app.
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // Open the DevTools.
+  win.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  win.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+
+  win.on("resize", () => {
     winSize = win.getSize();
-
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
-
-    // Open the DevTools.
-    // win.webContents.openDevTools();
-
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null
-    });
-
-    win.on('resize', () => {
-        winSize = win.getSize();
-    });
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
-
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow()
-    }
+app.on("window-all-closed", () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-
-ipcMain.on('parallax_initialized', () => {
-    let i = 400;
-    let rev = false;
-    // Simulate mouse move
-    setInterval(function() {
-        // Get window size
-        console.log('Window Size -->', winSize);
-        console.log('Mouse X index -->', i);        
-        win.webContents.sendInputEvent({type: 'mouseMove', x: i, y: Math.floor(winSize[1]/2)});
-        if(i >= winSize[0]){
-            rev = true;
-        } else if(i <= 0) {
-            rev = false;
-        }
-        i = rev ? i - step : i + step;
-
-    }, 100);
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
 });
 
-//var prevData = parser;
+ipcMain.on("port_selected", (e, data) => {
+  // Try to open the selected port
+  try {
+    var port = new SerialPort(data, {
+      baudRate: 9600
+    });
+    const parser = new Readline();
+    port.pipe(parser);
+    // Throw error to the UI
+    port.on("error", err => {
+      e.sender.send("error", err.toString());
+    });
+    port.on("open", () => {
+        e.sender.send("port_open");
+    });
+    // Move
+    parser.on("data", data => {
+        // Do your processing here.
+        console.log(data);
+    });
+  } catch (err) {
+    e.sender.send("error", err.toString());
+  }
+});
 
-// Switches the port into "flowing mode"
-// port.on('data', function (data) {
-//     console.log('Data:', data.toJSON());
-// });
-   
-  // Read data that is available but keep the stream from entering "flowing mode"
-// port.on('readable', function () {
-//     var data = port.read(8);
-//     console.log(data ? data.toString() : '');
-// });
+ipcMain.on("app_start", e => {
+  SerialPort.list()
+    .then(ports => {
+      e.sender.send("ports_data", ports);
+    })
+    .catch(err => {
+      e.sender.send("error", err.toString());
+    });
+});
+
+ipcMain.on("parallax_initialized", () => {
+  let i = 400;
+  let rev = false;
+  // Simulate mouse move
+  setInterval(function() {
+    // Get window size
+    console.log("Window Size -->", winSize);
+    console.log("Mouse X index -->", i);
+    win.webContents.sendInputEvent({
+      type: "mouseMove",
+      x: i,
+      y: Math.floor(winSize[1] / 2)
+    });
+    if (i >= winSize[0]) {
+      rev = true;
+    } else if (i <= 0) {
+      rev = false;
+    }
+    i = rev ? i - step : i + step;
+  }, 100);
+});
--- renderer.js
+++ renderer.js
@@ -1,7 +1,51 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer } = require("electron");
 
-// Initialize parallax
-var scene = document.getElementById('scene');
-var parallax = new Parallax(scene);
-ipcRenderer.send('parallax_initialized');
+// Listen to the error events from the main and renderer threads
+ipcRenderer.on("error", function(e, data) {
+  // Log the info
+  console.log("Error message ->", data);
+  document.querySelector("#error").classList.remove("is-hidden");
+  document.querySelector("#error-body").innerHTML = data;
+});
 
+// Listen to the ports data event and populate the COM select picker
+ipcRenderer.on("ports_data", function(e, data) {
+  let portValues;
+  // Log the info
+  console.log("Port Data ->", data);
+  if (data && data.length) {
+    data.forEach(p => {
+      portValues += `<option value="${p.comName}">${p.comName}</option>`;
+    });
+    document.querySelector("#error").classList.add("is-hidden");
+    document.querySelector("#ports").innerHTML = `
+            <select id="ports-list" name="ports">
+                ${portValues}
+            </select>
+        `;
+  }
+});
+
+ipcRenderer.on("port_open", function() {
+  console.log("Port Opened!");
+  // Initialize parallax
+  document.querySelector(".serial-port").classList.add("is-hidden");
+  document.querySelector(".container-parallax").classList.remove("is-hidden");
+  var scene = document.getElementById("scene");
+  var parallax = new Parallax(scene);
+  ipcRenderer.send("parallax_initialized");
+});
+
+// Add submit even handler for port selection
+document.querySelector("#port-select").addEventListener("submit", function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector("#error").classList.add("is-hidden");
+  ipcRenderer.send(
+    "port_selected",
+    document.querySelector("#ports-list").value
+  );
+});
+
+// Notify the main thread to start the app
+ipcRenderer.send("app_start");
